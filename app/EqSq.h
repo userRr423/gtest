9cabb776a3e75828cefae1e8fded2646c43492a8
userRr423 <misterinkognito67@gmail.com>
2023-11-22 16:31:38 +0300

    i

---
+++ app/EqSq.h
@@ -0,0 +1,8 @@
+#ifndef EQSQ_H
+#define EQSQ_H
+
+int roots(double a, double b, double c);
+
+extern int val;
+
+#endif // EQSQ_H
---
+++ app/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "EqSq.h"
+
+
+int main()
+{
+    double j = roots(6, 6, 1);
+    printf("%lf", j);
+    printf("Hello World!\n");
+    return 1;
+}
